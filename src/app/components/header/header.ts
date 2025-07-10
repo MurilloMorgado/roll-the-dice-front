4531a211b5b0f22e9168a1e30bbae6209384b902
Murillo M <mumorgado@hotmail.com>
2025-07-10 17:32:39 -0300

    implementando o header

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,11 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+import { BadgeModule } from 'primeng/badge';
+import { AvatarModule } from 'primeng/avatar';
+import { InputTextModule } from 'primeng/inputtext';
+import { CommonModule } from '@angular/common';
+import { Ripple } from 'primeng/ripple';
+import { MenubarModule } from 'primeng/menubar';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [MenubarModule, BadgeModule, AvatarModule, InputTextModule, Ripple, CommonModule],
   templateUrl: './header.html',
   styleUrl: './header.css'
 })
-export class Header {
+export class Header implements OnInit {
+
+  items: MenuItem[] | undefined;
+
+  ngOnInit(): void {
+    this.items = [
+      {
+        label: 'Home',
+        icon: 'pi pi-home',
+      },
+      {
+        label: 'Projects',
+        icon: 'pi pi-search',
+        badge: '3',
+      },
+    ];
+  }
 
 }
